9dcce53fddd8d9be06317d224b353733278a44b4
Sara <Sarahakimi206@yahoo.com>
2024-07-12 12:50:21 +0200

    neue Klasse-Neue Objekt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,13 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        // Instanziierung -->Erzeugen eines Objektes
+        // System.out.println(new Cat());
 
-
+        //cat -->Referenzvariable|verweiss
+        //Cat -->Referenzetyp(Klasse)
+      Cat cat = new Cat();
+      System.out.println(cat);
     }
 
    
